25a943c75ea30aeaa538af0c136f440ca9e78c45
gujiu_21L <2897795436@qq.com>
2024-01-31 19:01:24 +0800

    xiaowang commit ReallySubmittedByXiaoWang.go file

---
+++ ReallySubmittedByXiaoWang.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var textInput string = "嘿嘿！这次一定是我小王提交的！！！"
+	fmt.println(textInput)
+}
+
--- xiaowangsimplycommit.java
+++
@@ -1,8 +0,0 @@
-
-public class xiaowangsimplycommit
-{
-	public static void main(String[] main){
-		System.out.println("这次是小王提交吧嘿嘿“);
-	}
-}
-
