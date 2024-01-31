8391b90ea8f3fc76adc8f32235768fa801a5f485
gujiu_21L <2897795436@qq.com>
2024-01-31 16:52:01 +0800

    xiaowang commit java file

---
+++ xiaowangsimplycommit.java
@@ -0,0 +1,8 @@
+
+public class xiaowangsimplycommit
+{
+	public static void main(String[] main){
+		System.out.println("这次是小王提交吧嘿嘿“);
+	}
+}
+
--- xiaowangtijiao.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main(void)
-{
-	cout<< "此文件由小王提交" <<endl;
-	return 0;
-}
