6eada14745d4e72824c4daa5bd4b47494a2a9eee
gujiu_21L <2897795436@qq.com>
2024-01-31 16:34:57 +0800

    xiaowang commit xiaowangtijiao.cpp

--- helloxiaowang.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	printf("Hello Xiaowang\n");
-	return 0;
-}
---
+++ xiaowangtijiao.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout<< "此文件由小王提交" <<endl;
+	return 0;
+}
